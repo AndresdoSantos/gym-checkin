6aa753fb4d2c5dbb508f212a56609cccef258218
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-06-14 00:29:37 -0300

    App

---
+++ src/App.tsx
@@ -0,0 +1,137 @@
+import dayjs from 'dayjs';
+import { Clock } from 'phosphor-react';
+import { useCallback, useEffect, useState } from 'react';
+
+type Check = {
+  type: 'out' | 'in';
+  date: Date;
+};
+
+function App() {
+  const handleSubmitCheckIn = useCallback(() => {
+    const oldCheckIn = window.localStorage.getItem('@academy:check-in');
+
+    const newCheckIn: Check = {
+      type: 'in',
+      date: new Date(),
+    };
+
+    if (oldCheckIn) {
+      const oldCheckInParsed = JSON.parse(oldCheckIn);
+
+      window.localStorage.setItem(
+        '@academy:check-in',
+        JSON.stringify([...oldCheckInParsed, newCheckIn])
+      );
+    } else {
+      window.localStorage.setItem(
+        '@academy:check-in',
+        JSON.stringify([newCheckIn])
+      );
+    }
+  }, []);
+
+  const handleSubmitCheckOut = useCallback(() => {
+    const oldCheckOut = window.localStorage.getItem('@academy:check-out');
+
+    const newCheckOut: Check = {
+      type: 'out',
+      date: new Date(),
+    };
+
+    if (oldCheckOut) {
+      const oldCheckOutParsed = JSON.parse(oldCheckOut);
+
+      window.localStorage.setItem(
+        '@academy:check-out',
+        JSON.stringify([...oldCheckOutParsed, newCheckOut])
+      );
+    } else {
+      window.localStorage.setItem(
+        '@academy:check-out',
+        JSON.stringify([newCheckOut])
+      );
+    }
+  }, []);
+
+  const [checkIn, setCheckIn] = useState<Check[]>([]);
+  const [checkOut, setCheckOut] = useState<Check[]>([]);
+
+  useEffect(() => {
+    const oldCheckIn = window.localStorage.getItem('@academy:check-in');
+
+    if (oldCheckIn) {
+      setCheckIn(JSON.parse(oldCheckIn));
+    }
+
+    const oldCheckOut = window.localStorage.getItem('@academy:check-out');
+
+    if (oldCheckOut) {
+      setCheckOut(JSON.parse(oldCheckOut));
+    }
+  }, []);
+
+  return (
+    <main className="h-screen w-screen py-5 bg-zinc-800">
+      <main className="flex flex-col items-center mx-auto max-w-4xl h-full px-4">
+        <header className="mr-auto">
+          <h1 className="text-white text-lg">Good night!</h1>
+          <p className="text-white text-xs uppercase font-light">
+            mr. andres dos santos
+          </p>
+        </header>
+
+        <header className="flex items-center justify-between space-x-4 w-full mt-8">
+          <button
+            className="flex items-center justify-center space-x-2 border h-10 w-full rounded-full bg-green-500 border-green-500"
+            onClick={handleSubmitCheckIn}
+          >
+            <p className="text-white text-xs uppercase">check in</p>
+            <Clock weight="bold" className="text-white" />
+          </button>
+
+          <button
+            className="flex items-center justify-center space-x-2 border h-10 w-full rounded-full bg-red-500 border-red-500"
+            onClick={handleSubmitCheckOut}
+          >
+            <p className="text-white text-xs uppercase">check out</p>
+            <Clock weight="bold" className="text-white" />
+          </button>
+        </header>
+
+        <div className="mr-auto mt-11 w-full">
+          <h1 className="text-white text-xs uppercase mb-4">
+            your checkin's and checkout's
+          </h1>
+
+          <section className="grid grid-cols-2">
+            <ul className="flex flex-col items-center justify-between w-full">
+              {checkIn.map((item) => (
+                <li key={item.toString()} className="w-full my-2 text-start">
+                  <p className="text-white text-xs font-light uppercase">
+                    {dayjs(item.date).format('DD MMMM YYYY HH:MM')}
+                  </p>
+                </li>
+              ))}
+            </ul>
+
+            <ul className="flex flex-col items-center w-full">
+              {checkOut.map((item) => (
+                <li className="w-full my-2 text-end">
+                  <p
+                    key={item.toString()}
+                    className="text-white text-xs font-light uppercase"
+                  >
+                    {dayjs(item.date).format(`DD MMMM YYYY HH:MM`)}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
+      </main>
+    </main>
+  );
+}
+
+export default App;
